e2a926e89e534f41e5580eb8d7229a806da26a8f
Brian Parker <btparker70@gmail.com>
2021-11-26 11:18:39 -0800

    revert changes

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ function main() {
   window.addEventListener("deviceorientation", onOrientationChange)
 
   // grab the video display of the camera
-  navigator.mediaDevices.getUserMedia({video:true})
+  navigator.mediaDevices.getUserMedia({video:{
+    facingMode: 'environment'
+  }})
   .then(function(signal) {
     const video = document.getElementById("myVideo");
     video.srcObject = signal;
